6c4f1d17dcfba4b7fc84bd9d82d24bc9b00a5b12
Bastien-Arlot <arlot.b@gmail.com>
2022-09-19 09:40:11 +0200

    interface ex

---
+++ ArrayAndListEx/InterfaceExercises.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAndListEx
+{
+    public interface IActivity
+    {
+        void Execute();
+    }
+
+    public interface IWorkflow
+    {
+        void Add(IActivity activity);
+        void Remove(IActivity activity);
+        IEnumerable<IActivity> GetActivities();
+
+    }
+
+    public class Workflow : IWorkflow
+    {
+        private readonly List<IActivity> _activities;
+
+        public Workflow()
+        {
+            _activities = new List<IActivity>();
+        }
+
+        public void Add(IActivity activity)
+        {
+            _activities.Add(activity);
+        }
+
+        public void Remove(IActivity activity)
+        {
+            _activities.Remove(activity);
+        }
+
+        public IEnumerable<IActivity> GetActivities()
+        {
+            return _activities;
+        }
+    }
+
+    public class WorkflowEngine
+    {
+        public void Run(IWorkflow workflow)
+        {
+
+            foreach(IActivity I in workflow.GetActivities())
+            {
+                I.Execute();
+            }
+        }
+    }
+
+ 
+
+
+
+}
