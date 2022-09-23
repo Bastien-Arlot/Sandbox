7435c7b208fd9a06b489861d5d793b27667da84b
Bastien-Arlot <arlot.b@gmail.com>
2022-09-23 09:30:57 +0200

    init crud and previous ex

--- ArrayAndListEx/Program.cs
+++ ArrayAndListEx/Program.cs
@@ -6,18 +6,21 @@ namespace ArrayAndListEx
     {
         static void Main(string[] args)
         {
-            var ex = new ExercicesList();
-            var stringExercise = new StringExercices();
-            var stackExercise = new StackExercise();
+            //var ex = new ExercicesList();
+            //var stringExercise = new StringExercices();
+            //var stackExercise = new StackExercise();
 
 
-            stackExercise.Push(1);
-            stackExercise.Push(2);
-            stackExercise.Push(3);
+            //stackExercise.Push(1);
+            //stackExercise.Push(2);
+            //stackExercise.Push(3);
 
-            Console.WriteLine(stackExercise.Pop());
-            Console.WriteLine(stackExercise.Pop());
-            Console.WriteLine(stackExercise.Pop());
+            //Console.WriteLine(stackExercise.Pop());
+            //Console.WriteLine(stackExercise.Pop());
+            //Console.WriteLine(stackExercise.Pop());
+
+            Workflow workflow = new Workflow();
+
 
 
 
